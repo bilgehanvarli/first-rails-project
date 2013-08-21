b0fba02d31b039ceb641573af781e8b0f2b9e8ac
halit <diqit4ll@gmail.com>
2013-08-21 14:37:24 +0300

    first upload to github repo

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  http_basic_authenticate_with name: "halit", password: "secret", except: [:index, :show]
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
